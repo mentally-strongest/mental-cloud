c79a21c1172d07e3f85be1cb5650d2dc6d88362b
nazik <nazikvasa@gmail.com>
2024-12-20 21:41:53 +0200

    updated view_file.html

--- app/views.py
+++ app/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 from .models import File
 from .forms import FileUploadForm
+import mimetypes
 
 
 @login_required
 def upload_file(request):
-    if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            file_instance = form.save(commit=False)
-            file_instance.user = request.user
-            file_instance.save()
-            return redirect('home')
-    else:
-        form = FileUploadForm()
-    return render(request, 'app/file_upload.html', {'form': form})
-
+    if request.method == 'POST' and request.FILES.get('file'):
+        uploaded_file = request.FILES['file']
+        file_instance = File(
+            original_name=uploaded_file.name,
+            user=request.user,
+        )
+        file_instance.save()
+        file_instance.file = uploaded_file
+        file_instance.save()
+        return redirect('home')
+    return redirect('home')
 
 @login_required
 def home(request):
     files = File.objects.filter(user=request.user)
     return render(request, 'app/home.html', {'files': files})
+
+
+@login_required
+def view_file(request, file_id):
+    file = get_object_or_404(File, id=file_id)
+    owner = file.user == request.user
+
+    if not owner and not file.is_public:
+        return render(request, 'app/file_not_found.html', {'message': 'This file is private or does not exist.'})
+
+    return render(request, 'app/view_file.html', {'file': file, 'owner': owner})
 
 
 @login_required
@@ -42,6 +55,7 @@ def change_privacy(request, file_id):
 
     return render(request, 'app/change_privacy.html', {'file': file})
 
+
 @login_required
 def delete_file(request, file_id):
     file = get_object_or_404(File, id=file_id, user=request.user)
@@ -49,4 +63,18 @@ def delete_file(request, file_id):
     return redirect('home')
 
 
+@login_required
+def download_file(request, file_id):
+    file = get_object_or_404(File, id=file_id)
 
+    if file.user != request.user and not file.is_public:
+        raise Http404('You do not have permission to download this file.')
+
+    file_path = file.file.path
+    file_name = file.original_name
+    content_type, _ = mimetypes.guess_type(file_path)
+
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type=content_type)
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        return response
